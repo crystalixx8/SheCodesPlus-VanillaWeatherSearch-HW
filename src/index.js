7aed1e0d95ac0791b866fa94615246cdb364ca44
Crystal Gayle <cegayle88@gmail.com>
2024-05-27 23:17:10 -0400

    Add api data for weather conditions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function formatDateAndTime() {
+function formatDateAndTime(date) {
   let days = [
     "Sunday",
     "Monday",
@@ -8,10 +8,9 @@ function formatDateAndTime() {
     "Friday",
     "Saturday",
   ];
-  let now = new Date();
-  let day = days[now.getDay()];
-  let hour = now.getHours();
-  let min = now.getMinutes();
+  let day = days[date.getDay()];
+  let hour = date.getHours();
+  let min = date.getMinutes();
 
   if (min < 10) {
     min = `0${min}`;
@@ -25,15 +24,40 @@ function formatDateAndTime() {
   return dayTimeFormat;
 }
 
-function searchCity(event) {
-  event.preventDefault();
-  let cityInput = document.querySelector(".search-input");
-  let city = document.querySelector(".current-city");
-  city.innerHTML = cityInput.value;
+function updateWeather(response) {
+  let cityElement = document.querySelector("#current-city");
+  let currTempElement = document.querySelector(".current-temperature-value");
+  let desciptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind");
+  let iconElement = document.querySelector(".current-temperature-icon");
+  let timeElement = document.querySelector("#current-date");
+  let date = new Date(response.data.time * 1000);
+
+  cityElement.innerHTML = response.data.city;
+  currTempElement.innerHTML = Math.round(response.data.temperature.current);
+  desciptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  iconElement.innerHTML = `<img src ="${response.data.condition.icon_url}"/>`;
+  timeElement.innerHTML = formatDateAndTime(date)
 }
 
-let currentDateAndTime = document.querySelector("#current-date");
-currentDateAndTime.innerHTML = formatDateAndTime();
+function searchCity(city) {
+  let key = "08dcad5234e0828f84o97f5ct047b8d4";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&unit=imperial`;
+
+  axios.get(apiURL).then(updateWeather);
+}
+
+function searchSubmit(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+
+  searchCity(searchInputElement.value);
+}
 
 let searchButton = document.querySelector("form");
-searchButton.addEventListener("submit", searchCity);
+searchButton.addEventListener("submit", searchSubmit);
+
+searchCity("Sydney");
